4d4f45a539fff110b6c55dac7ab39361744fe094
williamjbf <williamjbf@gmail.com>
2022-05-28 16:21:53 -0300

    created endpoint that lists all users

--- src/main/java/com/github/williamjbf/springbootproject/controller/UsersController.java
+++ src/main/java/com/github/williamjbf/springbootproject/controller/UsersController.java
@@ -1,15 +1,29 @@
 package com.github.williamjbf.springbootproject.controller;
 
+import com.github.williamjbf.springbootproject.model.Users;
+import com.github.williamjbf.springbootproject.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
 
-    @GetMapping
-    public String list(){
-        return "all users will be listed here";
+    @Autowired
+    private UsersRepository repository;
+
+    @GetMapping(value = "listAll")
+    @ResponseBody
+    public ResponseEntity<List<Users>> listAllUsers(){
+        List<Users> users = repository.findAll();
+
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
